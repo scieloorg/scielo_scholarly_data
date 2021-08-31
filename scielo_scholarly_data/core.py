400e349dc57a511507ff681f0bcb46af02467790
Rafael JPD <rafael.pezzuto@gmail.com>
2021-08-31 17:24:14 -0300

    adiciona método para converter string para espaço-alpha

--- scielo_scholarly_data/core.py
+++ scielo_scholarly_data/core.py
@@ -21,3 +21,20 @@ def convert_to_alpha_num_space(text, keep_chars={}, replace_with=' '):
     return ''.join(new_text)
 
 
+def convert_to_alpha_space(text, keep_chars={}, replace_with=' '):
+    """
+    Mantém em text apenas caracteres alfa (letras latinas) e espaços
+    Possibilita manter em text caracteres especiais na lista keep_chars
+
+    :param text: texto a ser tratado
+    :param keep_chars: set de caracteres a serem mantidos
+    :param replace_with: caracte a ser inserido quando não for alfa ou não estiver em keep_chars
+    :return: texto com apenas caracteres alpha e espaço mantidos (e especiais, caso indicado)
+    """
+    new_text = []
+    for character in text:
+        if character.isalpha() or character.isspace() or (character in keep_chars):
+            new_text.append(character)
+        else:
+            new_text.append(replace_with)
+    return ''.join(new_text)
