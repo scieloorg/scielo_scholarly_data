f73959b53ffa6dde3dd21244064c8baf09cc1678
Rafael JPD <rafael.pezzuto@gmail.com>
2021-08-31 17:21:28 -0300

    adiciona expressões regulares (issn, doi, parênteses)

---
+++ scielo_scholarly_data/values.py
@@ -0,0 +1,19 @@
+import re
+
+
+PATTERN_PARENTHESIS = re.compile(r'[-a-zA-ZÀ-ÖØ-öø-ÿ|0-9]*\([-a-zA-ZÀ-ÖØ-öø-ÿ|\W|0-9]*\)[-a-zA-ZÀ-ÖØ-öø-ÿ|0-9]*', re.UNICODE)
+
+# https://www.crossref.org/blog/dois-and-matching-regular-expressions/ (accessed on 2021/08/31)
+PATTERNS_DOI = [re.compile(pd) for pd in [
+    r'10.\d{4,9}/[-._;()/:A-Z0-9]+$',
+    r'10.1002/[^\s]+$',
+    r'10.\d{4}/\d+-\d+X?(\d+)\d+<[\d\w]+:[\d\w]*>\d+.\d+.\w+;\d$',
+    r'10.1207/[\w\d]+\&\d+_\d+$']
+]
+
+# https://en.wikipedia.org/wiki/International_Standard_Serial_Number (accessed on 2021/08/31)
+PATTERNS_ISSN = [re.compile(pi) for pi in [
+    r'^[0-9]{4}[0-9]{3}[0-9xX]$',
+    r'^[0-9]{4}-[0-9]{3}[0-9xX]$']
+]
+
