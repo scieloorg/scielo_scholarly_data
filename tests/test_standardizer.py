7c2425f962e222fbe39d4e716aa723cd8f1f1c81
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2021-10-05 21:31:57 -0300

    Teste para a remoção de caracteres especiais

--- tests/test_standardizer.py
+++ tests/test_standardizer.py
@@ -274,18 +274,17 @@ class TestStandardizer(unittest.TestCase):
 
         self.assertListEqual(expected_values, obtained_values)
 
-
-    def test_document_title_alpha_num_spaces(self):
+    def test_document_title_for_visualization_alpha_num_spaces(self):
         titles = {
             'INNOVACIÓN TECNOLÓGICA EN LA RESOLUCIÓN DÊ: PROBLEMÁTICAS':
-                'INNOVACION TECNOLOGICA EN LA RESOLUCION DE PROBLEMATICAS',
+                'INNOVACIÓN TECNOLÓGICA EN LA RESOLUCIÓN DÊ PROBLEMÁTICAS',
             'INNOVACIÓN TECNOLÓGICA EN LA RESOLUCIÓN DẼ* PROBLEMÁTICAS':
-                'INNOVACION TECNOLOGICA EN LA RESOLUCION DE PROBLEMATICAS',
+                'INNOVACIÓN TECNOLÓGICA EN LA RESOLUCIÓN DẼ PROBLEMÁTICAS',
             'INNOVACIÓN TECNOLÓGICA EN LA RESOLUCIÓN DÈ& PROBLEMÁTICAS':
-                'INNOVACION TECNOLOGICA EN LA RESOLUCION DE PROBLEMATICAS'
+                'INNOVACIÓN TECNOLÓGICA EN LA RESOLUCIÓN DÈ PROBLEMÁTICAS'
         }
         expected_values = list(titles.values())
-        obtained_values = [document_title(dt) for dt in titles]
+        obtained_values = [document_title_for_visualization(dt) for dt in titles]
 
         self.assertListEqual(expected_values, obtained_values)
 
