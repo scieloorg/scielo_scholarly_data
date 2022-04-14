1df4f8d599275610528309be17f7f6a12af46803
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2022-04-14 15:19:39 -0300

    Adiciona método que utiliza similaridade semântica de textos

--- scielo_scholarly_data/sponsor.py
+++ scielo_scholarly_data/sponsor.py
@@ -2,9 +2,12 @@
 
 import scielo_scholarly_data.standardizer as standardizer
 import kshingle as ks
+from sentence_transformers import SentenceTransformer, util
+from collections import OrderedDict
+from operator import itemgetter
 
+model = SentenceTransformer('scielo_scholarly_data/stmodels/paraphrase-multilingual-MiniLM-L12-v2')
 
-def get_similar(name, standard_names):
 def make_standard_sponsor(sponsor):
     """
     Função para montar uma lista de dicionários a partir de uma string que descreve o nome de um financiador e seu acrônimo.
@@ -55,6 +58,7 @@ def make_standard_sponsor(sponsor):
     ]
     return result
 
+
 def search_sponsors_by_jaccard_similarity(name, sponsors):
     """
     Procedimento para obter o nome completo e o acrônimo do financiador de uma pesquisa,
@@ -101,26 +105,54 @@ def search_sponsors_by_jaccard_similarity(name, sponsors):
         return sorted(result, key=itemgetter('score'), reverse=True)
 
 
-def get_sponsor_names(name, pairs):
+def search_sponsors_by_semantic_similarity(name, sponsors):
     """
-    Procedimento para submeter um nome de financiador não padronizado e uma lista de tuplas
-    (nome padronizado, acrônimo) para medição de similaridade.
+    Procedimento para obter o nome completo e o acrônimo do financiador de uma pesquisa,
+    considerando a similaridade baseada em semântica textual.
+    (https://www.sbert.net/examples/training/sts/README.html)
 
     Parameters
     ----------
     name : str
-        Nome do financiador para padronização.
-    pairs : list
-        Lista de tuplas: nome completo e acrônimo padronizado.
+        Nome da instituição financiadora, da forma que foi declarada, para padronização.
+    sponsors : list
+        Uma lista de dicionários nos quais a chave "text" descreve as possíveis combinações de nome e acrônimo.
 
     Returns
     -------
-    tuple
-        1º elemento: nome completo padronizado
-        2º elemento: acrônimo padronizado
+    list
+        Uma lista ordenada de dicionários nos quais os nomes e acrônimos são associados a uma medida de similaridade.
+        [{
+            "standard_name": "Conselho Nacional de Desenvolvimento Científico e Tecnológico",
+            "standard_acronym": "CNPq",
+            "score": 1.0
+        },
+        {
+            "standard_name": "Coordenação de Aperfeiçoamento de Pessoal de Nível Superior",
+            "standard_acronym": "CAPES",
+            "score": 0.01
+        },
+        {
+            "standard_name": "Fundação de Amparo à Pesquisa do Estado de São Paulo",
+            "standard_acronym": "FAPESP",
+            "score": 0.05
+        }]
     """
-    name = standardizer.document_sponsors(name)
-    for pair in pairs:
-        response = get_similar(name, pair)
-        if response[-2:] != ("", ""):
-            return response
+    query_embedding = model.encode(name, convert_to_tensor=True)
+    texts = [item["text"] for item in sponsors]
+    corpus_embeddings = model.encode(texts, convert_to_tensor=True)
+    search_hits = util.semantic_search(query_embedding, corpus_embeddings)
+    search_hits = search_hits[0]  # Get the hits for the first query
+
+    result = []
+
+    for hit in search_hits:
+        related_sponsor = sponsors[hit['corpus_id']]
+        d = {
+            "standard_name": related_sponsor['name'],
+            "standard_acronym": related_sponsor['acronym'],
+            "score": hit['score']
+        }
+        result.append(d)
+
+    return result
