9a7fbbd615fe0057189e8f71ce7f8cffbfb3ebde
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2022-05-16 16:54:33 -0300

    Importação de bibliotecas e padrões

---
+++ scielo_scholarly_data/dates.py
@@ -0,0 +1,13 @@
+import html
+import unicodedata
+import re
+from datetime import datetime
+
+from dateutil.parser import parse
+from scielo_scholarly_data.values import (
+    PATTERN_DATE,
+    PATTERN_PARENTHESIS,
+    PUNCTUATION_TO_REMOVE_FROM_TITLE_VISUALIZATION,
+    MONTHS_DICT,
+)
+
